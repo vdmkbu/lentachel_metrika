994dd55e19093f4d071dccec1c565380f774fbd5
Vadim Kororstelev <vdmkbu@ya.ru>
2019-06-13 17:40:04 +0500

    модели и миграции

---
+++ database/migrations/2019_06_13_122929_create_author_reports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuthorReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('author_reports', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTime('date');
+            $table->string('url');
+            $table->bigInteger('count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('author_reports');
+    }
+}
---
+++ database/migrations/2019_06_13_122945_create_title_reports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTitleReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('title_reports', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTime('date');
+            $table->string('title');
+            $table->bigInteger('count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('title_reports');
+    }
+}
